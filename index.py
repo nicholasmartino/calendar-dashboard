c43bca294fbf5dd8b32da90cf87691095535c137
Nicholas Martino <nicholasmartino@host214-99.vpn.ubc.ca>
2022-01-21 11:06:20 -0800

    Niricson calendar

--- index.py
+++ index.py
@@ -61,7 +61,8 @@ CALENDARS = {
     'elementslab': 'https://outlook.live.com/owa/calendar/8fd2720a-5774-41df-90e9-fa8ff7cc3cbf/1b14feb2-a841-4f74-ae70-496436c6da96/cid-59D903F0133C1A64/calendar.ics',
     'Portfolio': 'https://outlook.live.com/owa/calendar/00000000-0000-0000-0000-000000000000/491714b4-dc60-421d-9cf7-cb2f1eaf2b0f/cid-59D903F0133C1A64/calendar.ics',
     'TA Winter': 'https://outlook.live.com/owa/calendar/00000000-0000-0000-0000-000000000000/31ce62b1-d87f-4b9e-a3ac-1faabb505f38/cid-59D903F0133C1A64/calendar.ics',
-    'Proptech': 'https://outlook.live.com/owa/calendar/00000000-0000-0000-0000-000000000000/d10fa344-e4a3-43af-afa3-04b8f5894668/cid-59D903F0133C1A64/calendar.ics'
+    'Proptech': 'https://outlook.live.com/owa/calendar/00000000-0000-0000-0000-000000000000/d10fa344-e4a3-43af-afa3-04b8f5894668/cid-59D903F0133C1A64/calendar.ics',
+    'Niricson': 'https://outlook.live.com/owa/calendar/00000000-0000-0000-0000-000000000000/9bb81193-6da5-43f1-b347-a282cfb60a08/cid-59D903F0133C1A64/calendar.ics'
 }
 COLORS = {
     'PhD': 'cadetblue',
@@ -71,27 +72,30 @@ COLORS = {
     'elementslab': 'gold',
     'Portfolio': 'mediumpurple',
     'TA Winter': 'firebrick',
-    'Proptech': 'navy'
+    'Proptech': 'navy',
+    'Niricson': 'gray'
 }
 THEMES = {
-    'PhD': 'Research',
-    'MITACS': 'Research',
-    'elementslab': 'Research',
-    'TA Fall': 'Teaching',
+    'PhD': 'Education',
+    'MITACS': 'Education',
+    'elementslab': 'Education',
+    'TA Fall': 'Education',
     'UPAL': '',
     'Portfolio': '',
-    'TA Winter': 'Teaching',
-    'Proptech': 'Practice'
+    'TA Winter': 'Education',
+    'Proptech': 'Real Estate',
+    'Niricson': 'Infrastructure'
 }
 GOAL = {
-    'PhD': 8,
-    'MITACS': 8,
-    'elementslab': 10,
-    'TA Fall': 4,
-    'UPAL': 1,
-    'Portfolio': 8,
+    'PhD': 5,  # Data Science
+    'MITACS': 5,  # UX
+    'elementslab': 10,  # GIS Analysis
+    'TA Fall': 0,
+    'UPAL': 0,
+    'Portfolio': 0,
     'TA Winter': 2,
-    'Proptech': 10
+    'Proptech': 10,  # Full Stack
+    'Niricson': 20  # QA
 }
 
 chart_template = dict(
